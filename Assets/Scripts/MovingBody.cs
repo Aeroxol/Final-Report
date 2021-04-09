ed29efb7ebd633aaa496f8dc614e23f23fb874a0
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2021-04-09 13:50:30 +0900

    merging

---
+++ Assets/Scripts/MovingBody.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBody : MonoBehaviour
+{
+    private Vector3 velocity;
+    private Vector3 acceleration;
+    private float mass;
+    public float Mass
+    {
+        get { return mass; }
+        set { mass = value; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Vector3.zero;
+        acceleration = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        velocity = velocity + acceleration * Time.deltaTime;
+        gameObject.transform.Translate(velocity * Time.deltaTime);
+    }
+}
